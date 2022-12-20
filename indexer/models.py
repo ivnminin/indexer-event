649f191b2ac7f4fa971c494e8acb18bb510cab07
ivn <ivn@li.ru>
2022-12-20 16:27:34 +0300

    chore: update models

--- indexer/models.py
+++ indexer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Contract(models.Model):
@@ -7,4 +8,37 @@ class Contract(models.Model):
         TOKEN_SALE = 2
     contract_type = models.IntegerField(choices=ContractType.choices)
     name = models.CharField(max_length=100, unique=True)
-    address = models.CharField(max_length=42)
+    address = models.CharField(max_length=42, unique=True)
+
+
+class EventBought(models.Model):
+    buyer_address = models.CharField(max_length=42)
+    amount = models.CharField(max_length=255)
+    tx = models.CharField(max_length=255)
+
+
+class EventTransfer(models.Model):
+    _from = models.CharField(max_length=42)
+    to = models.CharField(max_length=42)
+    amount = models.CharField(max_length=255)
+    tx = models.CharField(max_length=255)
+
+
+class Event(models.Model):
+    class EventType(models.IntegerChoices):
+        BOUGHT = 1
+        TRANSFER = 2
+    event_type = models.IntegerField(choices=EventType.choices)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name="events")
+
+    def save(self, *args, **kwargs):
+        if Event.objects.filter(contract__address=self.contract.address, event_type=self.event_type).exists():
+            raise ValidationError('Event exists')
+        super().save(*args, **kwargs)
+
+    @property
+    def event(self):
+        if self.event_type == self.EventType.BOUGHT:
+            return EventBought
+        elif self.event_type == self.EventType.TRANSFER:
+            return EventTransfer
--- indexer_token_sale.py
+++ indexer_token_sale.py
@@ -2,7 +2,9 @@ import json
 import logging
 import sys
 import time
+import os
 
+import django
 from web3 import Web3, middleware
 
 logging.basicConfig(
@@ -10,6 +12,7 @@ logging.basicConfig(
     format="%(levelname)8s %(asctime)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 FILENAME_CONTRACTS = 'contracts/contracts.json'
 FILENAME_ABI_TOKEN_SALE = 'contracts/artifacts/contracts/TokenSale.sol/TokenSale.json'
@@ -24,6 +27,9 @@ def read_file(filename):
 
 
 if __name__ == '__main__':
+    django.setup()
+    from indexer.models import Contract, EventBought, Event
+
     logging.info(f'start indexer {sys.argv[0]}')
 
     w3 = Web3(Web3.HTTPProvider(
@@ -32,28 +38,52 @@ if __name__ == '__main__':
     ))
     w3.middleware_onion.inject(middleware.geth_poa_middleware, layer=0)
 
-    token_contract = w3.eth.contract(
+    contract_token_sale = w3.eth.contract(
         abi=read_file(FILENAME_ABI_TOKEN_SALE)['abi'],
         address=Web3.toChecksumAddress(read_file(FILENAME_CONTRACTS)['token_sale'])
+    )
+
+    contract, _ = Contract.objects.get_or_create(
+        address=contract_token_sale.address,
+        defaults={
+            'contract_type': Contract.ContractType.TOKEN_SALE,
+            'name': 'Tone Sale',
+            'address': contract_token_sale.address
+        },
+    )
+    event_type_bought, _ = Event.objects.get_or_create(
+        event_type=Event.EventType.BOUGHT,
+        defaults={
+            'event_type': Event.EventType.BOUGHT,
+            'contract': contract,
+        },
     )
 
     from_block = START_BLOCK
     to_block = START_BLOCK + STEP
 
     while True:
+        logging.info(f'get events from {from_block} to {to_block}')
         logging.info(f'sleep {TIME_SLEEP} ...')
         time.sleep(TIME_SLEEP)
 
         last_block = w3.eth.get_block('latest')['number']
         if to_block > last_block:
             continue
-
-        events = token_contract.events.Bought.createFilter(
+        events = contract_token_sale.events.Bought.createFilter(
             fromBlock=from_block,
             toBlock=to_block
         ).get_all_entries()
 
-        logging.info(f'number of events : {len(events)}')
+        logging.info(f'    number of events : {len(events)}')
 
         for event in events:
-            print(event)
+            logging.info(f'    save tx {event.transactionHash.hex()}')
+            EventBought.objects.create(
+                buyer_address=event.args._buyer,
+                amount=event.args._amount,
+                tx=event.transactionHash.hex()
+            )
+
+        from_block = to_block + 1
+        to_block = from_block + STEP
